71f9e2258cc5e604cc9baf875cfb9d0af60175d6
blackleg <hectorespertpardo@gmail.com>
2019-01-04 18:55:37 +0100

    Add cypress tests

---
+++ tests/e2e/specs/test.js
@@ -0,0 +1,10 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('Test paypal button', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/')
+    cy.get('.braintree-paypal-button').children().should('have.length', 0)
+    cy.get('input').type('sandbox_85mbycg5_f47gdh83pzbn5nbc')
+    cy.get('.braintree-paypal-button').children().should('have.length', 1)
+  })
+})
