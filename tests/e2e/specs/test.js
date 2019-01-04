fbe07ade714a7f074f36cbb4e668e7214d045be2
blackleg <hectorespertpardo@gmail.com>
2019-01-04 18:57:34 +0100

    Fix javascript lint

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,9 +2,9 @@
 
 describe('Test paypal button', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.get('.braintree-paypal-button').children().should('have.length', 0)
-    cy.get('input').type('sandbox_85mbycg5_f47gdh83pzbn5nbc')
-    cy.get('.braintree-paypal-button').children().should('have.length', 1)
-  })
-})
+    cy.visit('/');
+    cy.get('.braintree-paypal-button').children().should('have.length', 0);
+    cy.get('input').type('sandbox_85mbycg5_f47gdh83pzbn5nbc');
+    cy.get('.braintree-paypal-button').children().should('have.length', 1);
+  });
+});
